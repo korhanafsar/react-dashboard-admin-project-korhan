890ce316afedb88e278fa4df7184008c2fe7fbbf
Khorkhan <korhanafsar@gmail.com>
2025-02-10 00:25:39 +0300

    İlk proje yüklemesi

---
+++ src/utils/fackData/leadTableData.js
@@ -0,0 +1,188 @@
+const status = [
+    { value: 'active', label: 'Active', color: '#17c666' },
+    { value: 'inactive', label: 'Inactive', color: '#ffa21d' },
+    { value: 'declined', label: 'Declined', color: '#ea4d4d' },
+];
+
+export const leadTableData = [
+    {
+        "id": 1,
+        "customer": {
+            "name": "Korhan Afsar",
+            "img": "/images/avatar/1.png"
+        },
+        "email": "korhanafsar@gmail.com",
+        "source": {
+            "media": "facebook",
+            "icon": "feather-facebook"
+        },
+        "phone": "+1 (375) 9632 548",
+        "date": "2025-04-25, 03:42PM",
+        "status": { status, defaultSelect: 'active' }
+    },
+    {
+        "id": 2,
+        "customer": {
+            "name": "Korhan Afsar",
+            "img": ""
+        },
+        "email": "korhanafsar@gmail.com",
+        "source": {
+            "media": "facebook",
+            "icon": "feather-facebook"
+        },
+        "phone": "(375) 8523 456",
+        "date": "2025-04-06, 02:52PM",
+        "status": { status, defaultSelect: 'active' }
+    },
+    {
+        "id": 3,
+        "customer": {
+            "name": "Korhan Afsar",
+            "img": "/images/avatar/2.png"
+        },
+        "email": "korhanafsar@gmail.com",
+        "source": {
+            "media": "Twitter",
+            "icon": "feather-twitter"
+        },
+        "phone": "(845) 9632 874",
+        "date": "2025-04-08, 08:34PM",
+        "status": { status, defaultSelect: 'active' }
+    },
+    {
+        "id": 4,
+        "customer": {
+            "name": "Korhan Afsar",
+            "img": "/images/avatar/3.png"
+        },
+        "email": "korhanafsar@gmail.com",
+        "source": {
+            "media": "Linkedin",
+            "icon": "feather-linkedin"
+        },
+        "phone": "(258) 9514 657",
+        "date": "2025-04-10, 05:25PM",
+        "status": { status, defaultSelect: 'inactive' }
+    },
+    {
+        "id": 5,
+        "customer": {
+            "name": "Korhan Afsar",
+            "img": "/images/avatar/7.png"
+        },
+        "email": "korhanafsar@gmail.com",
+        "source": {
+            "media": "Instagram",
+            "icon": "feather-instagram"
+        },
+        "phone": "(456) 6547 524",
+        "date": "2025-04-12, 12:02PM",
+        "status": { status, defaultSelect: 'active' }
+    },
+    {
+        "id": 6,
+        "customer": {
+            "name": "Korhan Afsar",
+            "img": "/images/avatar/4.png"
+        },
+        "email": "korhanafsar@gmail.com",
+        "source": {
+            "media": "Github",
+            "icon": "feather-github"
+        },
+        "phone": "(375) 8523 456",
+        "date": "2025-04-15, 02:40PM",
+        "status": { status, defaultSelect: 'active' }
+    },
+    {
+        "id": 7,
+        "customer": {
+            "name": "Korhan Afsar",
+            "img": "/images/avatar/5.png"
+        },
+        "email": "korhanafsar@gmail.com",
+        "source": {
+            "media": "facebook",
+            "icon": "feather-facebook"
+        },
+        "phone": "(632) 5486 662",
+        "date": "2025-04-25, 03:42PM",
+        "status": { status, defaultSelect: 'active' }
+    },
+    {
+        "id": 8,
+        "customer": {
+            "name": "Korhan Afsar",
+            "img": "/images/avatar/6.png"
+        },
+        "email": "korhanafsar@gmail.com",
+        "source": {
+            "media": "Linkedin",
+            "icon": "feather-linkedin"
+        },
+        "phone": "(951) 5478 884",
+        "date": "2025-04-14, 03:32PM",
+        "status": { status, defaultSelect: 'declined' }
+    },
+    {
+        "id": 9,
+        "customer": {
+            "name": "Korhan Afsar",
+            "img": ""
+        },
+        "email": "korhanafsar@gmail.com",
+        "source": {
+            "media": "Twitter",
+            "icon": "feather-twitter"
+        },
+        "phone": "(556) 2457 586",
+        "date": "2025-04-20, 01:47PM",
+        "status": { status, defaultSelect: 'active' }
+    },
+    {
+        "id": 10,
+        "customer": {
+            "name": "Korhan Afsar",
+            "img": "/images/avatar/4.png"
+        },
+        "email": "korhanafsar@gmail.com",
+        "source": {
+            "media": "Github",
+            "icon": "feather-github"
+        },
+        "phone": "(554) 2478 663",
+        "date": "2025-04-22, 02:12PM",
+        "status": { status, defaultSelect: 'active' }
+    },
+    {
+        "id": 11,
+        "customer": {
+            "name": "Korhan Afsar",
+            "img": ""
+        },
+        "email": "korhanafsar@gmail.com",
+        "source": {
+            "media": "Instagram",
+            "icon": "feather-instagram"
+        },
+        "phone": " (654) 2478 665",
+        "date": "2025-04-25, 03:42PM",
+        "status": { status, defaultSelect: 'declined' }
+    },
+    {
+        "id": 12,
+        "customer": {
+            "name": "Elliot Nancy",
+            "img": "/images/avatar/9.png"
+        },
+        "email": "korhanafsar@gmail.com",
+        "source": {
+            "media": "Instagram",
+            "icon": "feather-instagram"
+        },
+        "phone": "(554) 2478 663",
+        "date": "2025-04-25, 03:42PM",
+        "status": { status, defaultSelect: 'inactive' }
+    },
+]
---
+++ src/utils/fackData/recentDealData.js
@@ -0,0 +1,31 @@
+export const recentDealData = [
+    {
+        deal_name: "Web developement deal with alex",
+        deal_price: "450",
+        dealr_name: "Korhan Afsar",
+        dealr_img: "/images/avatar/1.png",
+        clossing_date: "22 March, 2025"
+    },
+    {
+        deal_name: "Web developement deal with alex",
+        deal_price: "550",
+        dealr_name: "Korhan Afsar",
+        dealr_img: "/images/avatar/2.png",
+        clossing_date: "22 March, 2025"
+    },
+    {
+        deal_name: "Web developement deal with alex",
+        deal_price: "850",
+        dealr_name: "Korhan Afsar",
+        dealr_img: "/images/avatar/3.png",
+        clossing_date: "22 March, 2025"
+    },
+    {
+        deal_name: "Web developement deal with alex",
+        deal_price: "900",
+        dealr_name: "Korhan Afsar",
+        dealr_img: "/images/avatar/4.png",
+        clossing_date: "22 March, 2025"
+    },
+
+]
